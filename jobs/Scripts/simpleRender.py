61ebd097bb3ab91c1e752084b0a52f0a665646e4
Ilia Nemankov <INemankov@luxoft.com>
2021-05-06 16:51:13 +0300

    Init commit

---
+++ jobs/Scripts/simpleRender.py
@@ -0,0 +1,275 @@
+import argparse
+import os
+import subprocess
+import psutil
+import json
+import platform
+from datetime import datetime
+from shutil import copyfile, move, which
+import sys
+from utils import is_case_skipped
+from queue import Queue
+from subprocess import PIPE, Popen
+from threading import Thread
+import copy
+
+sys.path.append(os.path.abspath(os.path.join(
+    os.path.dirname(__file__), os.path.pardir, os.path.pardir)))
+import jobs_launcher.core.config as core_config
+from jobs_launcher.core.system_info import get_gpu
+
+
+def copy_test_cases(args):
+    try:
+        copyfile(os.path.realpath(os.path.join(os.path.dirname(
+            __file__), '..', 'Tests', args.test_group, 'test_cases.json')),
+            os.path.realpath(os.path.join(os.path.abspath(
+                args.output), 'test_cases.json')))
+
+        cases = json.load(open(os.path.realpath(
+            os.path.join(os.path.abspath(args.output), 'test_cases.json'))))
+
+        with open(os.path.join(os.path.abspath(args.output), "test_cases.json"), "r") as json_file:
+            cases = json.load(json_file)
+
+        if os.path.exists(args.test_cases) and args.test_cases:
+            with open(args.test_cases) as file:
+                test_cases = json.load(file)['groups'][args.test_group]
+                if test_cases:
+                    necessary_cases = [
+                        item for item in cases if item['case'] in test_cases]
+                    cases = necessary_cases
+
+        # create two cases: one for HybridPro, one for Northstar64
+        duplicated_cases = []
+        for case in cases:
+            for plugin in ["HybridPro", "Northstar64"]
+                copied_case = copy.deepcopy(case)
+                copied_case["plugin"] = plugin
+                duplicated_cases.append(copied_case)
+
+        with open(os.path.join(args.output, 'test_cases.json'), "w+") as file:
+            json.dump(duplicated_cases, file, indent=4)
+    except Exception as e:
+        core_config.logging.error('Can\'t load test_cases.json')
+        core_config.main_logger.error(str(e))
+        group_failed(args)
+        exit(-1)
+
+
+def prepare_empty_reports(args, current_conf):
+    main_logger.info('Create empty report files')
+
+    copyfile(os.path.abspath(os.path.join(args.output, '..', '..', '..', '..', 'jobs_launcher',
+                                          'common', 'img', 'error.jpg')), os.path.join(args.output, 'Color', 'failed.jpg'))
+
+    with open(os.path.join(os.path.abspath(args.output), "test_cases.json"), "r") as json_file:
+        cases = json.load(json_file)
+
+    for case in cases:
+        if utils.is_case_skipped(case, current_conf):
+            case['status'] = 'skipped'
+
+        if case['status'] != 'done' and case['status'] != 'error':
+            if case["status"] == 'inprogress':
+                case['status'] = 'active'
+
+            test_case_report = RENDER_REPORT_BASE.copy()
+            test_case_report['test_case'] = case['case']
+            test_case_report['render_device'] = get_gpu()
+            test_case_report['script_info'] = case['script_info']
+            test_case_report['geometry'] = case['geometry']
+            test_case_report['material_file'] = case['material_file']
+            test_case_report['material_path'] = case['material_path']
+            test_case_report['iterations'] = int(case.get('iterations', 10))
+            test_case_report['test_group'] = args.test_group
+            test_case_report['date_time'] = datetime.now().strftime(
+                '%m/%d/%Y %H:%M:%S')
+            if case['status'] == 'skipped':
+                test_case_report['test_status'] = 'skipped'
+                test_case_report['file_name'] = case['case'] + case.get('extension', '.jpg')
+                test_case_report['render_color_path'] = os.path.join('Color', test_case_report['file_name'])
+                test_case_report['group_timeout_exceeded'] = False
+
+                try:
+                    skipped_case_image_path = os.path.join(args.output, 'Color', test_case_report['file_name'])
+                    if not os.path.exists(skipped_case_image_path):
+                        copyfile(os.path.join(args.output, '..', '..', '..', '..', 'jobs_launcher', 
+                            'common', 'img', "skipped.jpg"), skipped_case_image_path)
+                except OSError or FileNotFoundError as err:
+                    main_logger.error("Can't create img stub: {}".format(str(err)))
+            else:
+                test_case_report['test_status'] = 'error'
+                test_case_report['file_name'] = 'failed.jpg'
+                test_case_report['render_color_path'] = os.path.join('Color', 'failed.jpg')
+
+            case_path = os.path.join(args.output, case['case'] + CASE_REPORT_SUFFIX)
+
+            if os.path.exists(case_path):
+                with open(case_path) as f:
+                    case_json = json.load(f)[0]
+                    test_case_report["number_of_tries"] = case_json["number_of_tries"]
+
+            with open(case_path, "w") as f:
+                f.write(json.dumps([test_case_report], indent=4))
+
+        copy_baselines(args, case, baseline_path, baseline_path_tr)
+    with open(os.path.join(args.output, "test_cases.json"), "w+") as f:
+        json.dump(cases, f, indent=4)
+
+
+def read_output(pipe, functions):
+    for line in iter(pipe.readline, b''):
+        for function in functions:
+            function(line.decode('utf-8'))
+    pipe.close()
+
+
+def save_results(args, case, cases, test_case_status, render_time, error_messages = []):
+    with open(os.path.join(args.output, case["case"] + CASE_REPORT_SUFFIX), "r") as file:
+        test_case_report = json.loads(file.read())[0]
+        test_case_report["file_name"] = case["case"] + case.get("extension", '.jpg')
+        test_case_report["test_status"] = test_case_status
+        test_case_report["render_time"] = render_time
+        test_case_report["render_log"] = os.path.join("render_tool_logs", test["case"] + ".log")
+        test_case_report["testing_start"] = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+        test_case_report["number_of_tries"] += 1
+
+        if test_case_status == "passed":
+            test_case_report["render_color_path"] = os.path.join("Color", test_case_report["file_name"])
+        else:
+            test_case_report["message"] = list(error_messages)
+
+        if test_case_status == "passed" or test_case_status == "error":
+            test_case_report["group_timeout_exceeded"] = False
+
+    with open(os.path.join(args.output, case["case"] + CASE_REPORT_SUFFIX), "w") as file:
+        json.dump([test_case_report], file, indent=4)
+
+    case["status"] = test_case_status
+    with open(os.path.join(args.output, "test_cases.json"), "w") as file:
+        json.dump(cases, file, indent=4)
+
+
+def execute_tests(args, current_conf):
+    rc = 0
+
+    with open(os.path.join(os.path.abspath(args.output), "test_cases.json"), "r") as json_file:
+        cases = json.load(json_file)
+
+    for case in [x for x in cases if not utils.is_case_skipped(x, current_conf)]:
+
+        current_try = 0
+
+        error_messages = set()
+
+        while current_try < args.retries:
+            try:
+                execution_script = """{tool} --plugin {plugin} --geometry {geometry} --material {material} 
+                --path {path} --iterations {iterations} --output {output}"""
+
+                execution_script = execution_script.format(tool=args.tool, plugin=case["plugin"], 
+                    geometry=case["geometry"], material=case["material_file"], path=case["material_path"], 
+                    iterations=case["iterations"], output=case["case"] + case.get("extension", ".jpg"))
+
+                execution_script_path = os.path.join(args.output, "{}.bat".format(case_name))
+       
+                with open(execution_script_path, "w") as f:
+                    f.write(execution_script)
+
+                status = "error"
+
+                p = psutil.Popen(execution_script_path, shell=True,
+                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+                outs = []
+                errs = []
+                queue = Queue()
+
+                stdout_thread = Thread(target=read_output, args=(p.stdout, [queue.put, outs.append]))
+                stderr_thread = Thread(target=read_output, args=(p.stderr, [queue.put, errs.append]))
+
+                start_time = time.time()
+
+                for thread in (stdout_thread, stderr_thread):
+                    thread.daemon = True
+                    thread.start()
+
+                try:
+                    p.wait(timeout=args.timeout)
+
+                    status = "passed"
+
+                    render_time = time.time() - start_time
+
+                    save_results(args, case, cases, "passed", render_time)
+                except (psutil.TimeoutExpired, subprocess.TimeoutExpired) as err:
+                    core_config.main_logger.error("Test case {} has been aborted by timeout".format(case["case"]))
+                    for child in reversed(p.children(recursive=True)):
+                        child.terminate()
+                    p.terminate()
+                finally:
+                    with open(os.path.join(args.output, json_name), "w") as f:
+                        json.dump(report, f, indent=4)
+
+                    outs = " ".join(outs)
+                    errs = " ".join(errs)
+
+                    with open(log_path, "w", encoding="utf-8") as file:
+                        file.write(outs)
+                        file.write(errs)
+
+                break
+            except Exception as e:
+                save_results(args, case, cases, "failed", -0.0, error_messages = error_messages)
+                error_messages.add(str(e))
+                utils.case_logger.error("Failed to execute test case (try #{}): {}".format(current_try, str(e)))
+                utils.case_logger.error("Traceback: {}".format(traceback.format_exc()))
+        else:
+            utils.case_logger.error("Failed to execute case '{}' at all".format(case["case"]))
+            rc = -1
+            save_results(args, case, cases, "error", -0.0, error_messages = error_messages)
+
+    return rc
+
+
+def createArgsParser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--tool", required=True, metavar="<path>")
+    parser.add_argument("--output", required=True, metavar="<dir>")
+    parser.add_argument("--test_group", required=True)
+    parser.add_argument("--res_path", required=True)
+    parser.add_argument("--test_cases", required=True)
+    parser.add_argument("--retries", required=False, default=2, type=int)
+    parser.add_argument("--update_refs", required=True)
+    parser.add_argument('--timeout', required=False, default=300)
+
+    return parser
+
+
+if __name__ == '__main__':
+    core_config.main_logger.info('simpleRender start working...')
+
+    args = createArgsParser().parse_args()
+
+    try:
+        os.makedirs(args.output)
+
+        if not os.path.exists(os.path.join(args.output, "render_tool_logs")):
+            os.makedirs(os.path.join(args.output, "render_tool_logs"))
+
+        render_device = get_gpu()
+        system_pl = platform.system()
+        current_conf = set(platform.system()) if not render_device else {platform.system(), render_device}
+        main_logger.info("Detected GPUs: {}".format(render_device))
+        main_logger.info("PC conf: {}".format(current_conf))
+        main_logger.info("Creating predefined errors json...")
+
+        copy_test_cases(args)
+        prepare_empty_reports(args, current_conf)
+        exit(execute_tests(args, current_conf))
+    except Exception as e:
+        main_logger.error("Failed during script execution. Exception: {}".format(str(e)))
+        main_logger.error("Traceback: {}".format(traceback.format_exc()))
+        exit(-1)
---
+++ local_config.py
@@ -0,0 +1,5 @@
+tool_name = 'hybrid_vs_ns'
+report_type = 'default'
+show_skipped_groups = True
+tracked_metrics = {}
+tracked_metrics_files_number = 10
